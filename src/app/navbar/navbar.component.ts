f02589196c8546741bd035093f8baf84039994aa
thusbandUK <106232364+thusbandUK@users.noreply.github.com>
2024-07-22 10:12:29 +0100

    target self added to hashtag links in navbar

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -14,16 +14,16 @@ import { Component } from '@angular/core';
         <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
           <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
             <li class="nav-item px-lg-4 mx-lg-1">
-              <a class="nav-link" aria-current="page" href="#about-me">About</a>
+              <a class="nav-link" aria-current="page" href="#about-me" target="_self">About</a>
             </li>
             <li class="nav-item px-lg-4 mx-lg-1">
               <a class="nav-link" href="#projects" target="_self">Projects</a>
             </li>
             <li class="nav-item px-lg-4 mx-lg-1">
-              <a class="nav-link" href="#bonus-material">Bonus Material</a>
+              <a class="nav-link" href="#bonus-material" target="_self">Bonus Material</a>
             </li>
             <li class="nav-item px-lg-4 mx-lg-1">
-              <a class="nav-link" href="#get-in-touch">Contact</a>
+              <a class="nav-link" href="#get-in-touch" target="_self">Contact</a>
             </li>
             <li class="nav-item px-lg-4 mx-lg-3">
               <a class="nav-link" href="./files/CV.pdf" target="_blank" download="THusband-CV">CV</a>
@@ -33,8 +33,7 @@ import { Component } from '@angular/core';
             </li>                       
             <li class="nav-item p-lg-0">
               <a class="nav-link px-lg-1" aria-label="link to Github account" title="link to Github account" href="https://github.com/thusbandUK" target="_blank"><i class="fa-brands fa-lg fa-github"></i></a>              
-            </li>          
-            
+            </li>
           </ul>          
         </div>
       </div>
