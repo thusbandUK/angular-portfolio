994bc02824efca2723da4666ae8f01de501a715a
thusbandUK <106232364+thusbandUK@users.noreply.github.com>
2024-08-13 19:50:52 +0100

    removed script references from angular json file for js scripts deleted in prev commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, HostListener, ElementRef, ViewChild, AfterViewInit, OnChanges } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
